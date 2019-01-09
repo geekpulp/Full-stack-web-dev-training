bf281ddb567db33816fe3382e31dcd7bbc9fd556
Glen Young <glen@geekpulp.co.nz>
2019-01-10 06:32:15 +1300

    add guessing game

---
+++ JavaScript/Guessing game/main.js
@@ -0,0 +1,16 @@
+alert("Hello");
+// create a secret number
+var secretNumber = 4;
+// ask user for guess
+var stringGuess = prompt("Guess a number");
+var guess = Number(stringGuess);
+// check guess
+if(guess === secretNumber) {
+    alert("Correct");
+}
+else if (guess > secretNumber) {
+    alert("Too high, guess again");
+}
+else {
+    alert("Too low, guess again");
+}
