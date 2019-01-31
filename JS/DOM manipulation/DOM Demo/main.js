d5bbe5c28e5238a3faa88060ee134a338fefe4d1
Glen Young <glen@geekpulp.co.nz>
2019-02-01 06:23:20 +1300

    add event listening

--- JS/DOM manipulation/DOM Demo/main.js
+++ JS/DOM manipulation/DOM Demo/main.js
@@ -4,9 +4,36 @@ var ul = document.querySelector( "ul" );
 p.textContent = "This is the business";
 
 // first select the element
+var button = document.querySelector( "button" );
 var heading = document.querySelector( "h1" );
 // then add the listener
-heading.addEventListener( "click", function () {
+button.addEventListener( "click", function () {
     console.log( "SOMEONE CLICKED A HEADING!" );
-    heading.style.fontSize = "100px";
+    heading.textContent = "Someone clicked the button!";
+} );
+
+// This is a common pattern in interaction. 
+var listItems = document.querySelectorAll( "li" );
+for ( var i = 0; i < listItems.length; i++ ) {
+    listItems[ i ].addEventListener( "click", function () {
+        this.style.color = "pink";
+    } );
+}
+
+//this is a good example for a blog post
+// var buttons = document.querySelectorAll( "button" );
+// var background = document.querySelector( "body" );
+
+// buttons[ 1 ].addEventListener( "click", function () {
+//     if ( background.style.background === "white" ) {
+//         background.style.background = "purple";
+//     } else {
+//         background.style.background = "white";
+//     }
+// } );
+
+// a better way, always remember the separation of concerns
+var buttons = document.querySelectorAll( "button" );
+buttons[ 1 ].addEventListener( "click", function () {
+    document.body.classList.toggle( "purple" );
 } );
