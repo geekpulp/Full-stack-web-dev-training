1e6dcbdf5aaf35e77ac9d0f832f7e8f05a7f1e40
Glen Young <glen@geekpulp.co.nz>
2019-02-06 07:42:49 +1300

    add score keeper game

---
+++ JS/score keeper/main.js
@@ -0,0 +1,42 @@
+var playerOneScore = 0,
+    playerTwoScore = 0,
+    maxScore = document.querySelector( "#maxScoreInput" ).value,
+    maxScoreDisplay = document.querySelector( "#maxScore" )
+
+var playerOneButton = document.querySelector( "#playerOneButton" );
+playerOneButton.addEventListener( "click", function () {
+    score( "playerOne", playerOneScore )
+} );
+
+var playerTwoButton = document.querySelector( "#playerTwoButton" );
+playerTwoButton.addEventListener( "click", function () {
+    score( "playerTwo", playerTwoScore )
+} );
+
+var resetButton = document.querySelector( "#resetButton" );
+resetButton.addEventListener( "click", function () {
+    reset();
+} );
+
+function score( player, playerScore ) {
+    console.log( "player score: " + playerScore + " max score: " + maxScore );
+    if ( playerScore === maxScore ) {
+        alert( "Game over man, game over!" )
+    } else {
+        if ( player === "playerOne" ) {
+            playerOneScore += 1;
+            console.log( "Player One Score: " + playerOneScore );
+        } else {
+            playerTwoScore += 1;
+            console.log( "Player Two Score: " + playerTwoScore );
+        }
+    }
+    //check if the score is at the max score, if it is do nothing
+    //increase the player score by one
+    //update the display for player one by one then if it hits max score turn display green
+}
+
+function reset() {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+}
