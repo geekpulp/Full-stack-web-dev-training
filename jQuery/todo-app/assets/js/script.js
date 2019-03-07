f8a494d85f7577827c2889009b66120cf5a908fa
Glen Young <glen@geekpulp.co.nz>
2019-03-07 17:35:25 +1300

    add a code comment

--- jQuery/todo-app/assets/js/script.js
+++ jQuery/todo-app/assets/js/script.js
@@ -11,6 +11,7 @@ $( "ul" ).on( "click", "span", function ( event ) {
     event.stopPropagation();
 } );
 
+// when the user presses enter a new item is added to the todo list
 $( "input[type='text']" ).keypress( function ( event ) {
     if ( event.which === 13 ) {
         var todoItem = $( this ).val();
