cdae86f0d4eef3e2d5ff28ba6782e5186bf625f0
Glen Young <glen@geekpulp.co.nz>
2019-03-07 17:16:02 +1300

    Add add and remove new items to the todo list

--- jQuery/todo-app/assets/js/script.js
+++ jQuery/todo-app/assets/js/script.js
@@ -1,12 +1,20 @@
 // check off specific todos by clicking
-$( "li" ).click( function () {
+$( "ul" ).on( "click", "li", function () {
     $( this ).toggleClass( "completed" );
 } );
 
 // click on x to delete todo item
-$( "span" ).click( function ( event ) {
+$( "ul" ).on( "click", "span", function ( event ) {
     $( this ).parent().fadeOut( 300, function () {
         $( this ).remove();
     } );
     event.stopPropagation();
+} );
+
+$( "input[type='text']" ).keypress( function ( event ) {
+    if ( event.which === 13 ) {
+        var todoItem = $( this ).val();
+        $( this ).val( "" );
+        $( "ul" ).append( "<li><span>X</span> " + todoItem + "</li>" );
+    }
 } );
