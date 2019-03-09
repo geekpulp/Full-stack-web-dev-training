878dc2648d17ab207efbc4afaa5c331e7cd207e0
Glen Young <glen@geekpulp.co.nz>
2019-03-10 08:32:30 +1300

    Add styles for todo list app

--- jQuery/todo-app/assets/js/script.js
+++ jQuery/todo-app/assets/js/script.js
@@ -16,6 +16,10 @@ $( "input[type='text']" ).keypress( function ( event ) {
     if ( event.which === 13 ) {
         var todoItem = $( this ).val();
         $( this ).val( "" );
-        $( "ul" ).append( "<li><span>X</span> " + todoItem + "</li>" );
+        $( "ul" ).append( "<li><span><i class='far fa-trash-alt'></i></span> " + todoItem + "</li>" );
     }
+} );
+
+$( ".fa-plus" ).click( function () {
+    $( "input[type='text']" ).fadeToggle();
 } );
