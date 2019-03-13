c87da1aa425d5cc3744d367cc88a6d7d8b0f0112
Glen Young <glen@geekpulp.co.nz>
2019-03-13 16:56:05 +1300

    add first node efforts
    Just started on backend stuff. Giving node a go. not using cloud 9 as suggested. Just local dev environment seems to work just fine.

---
+++ Node/intro to node/exercise/echo.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function echo( message, numberOfTimes ) {
+  for ( let i = 0; i < numberOfTimes; i++ ) {
+    console.log( message );
+  }
+}
+
+echo( "Echo!!!", 10 );
+echo( "Tater Tots", 3 );
