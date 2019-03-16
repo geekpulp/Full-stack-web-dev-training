7c0209c4c7558f35210057c1dabb54690de62e83
Glen Young <glen@geekpulp.co.nz>
2019-03-17 06:55:16 +1300

    add first express app
    My first express app. It doesnt do much but its strangly exciting.

---
+++ Node/IntroToExpress/FirstExpressApp/app.js
@@ -0,0 +1,25 @@
+var express = require( "express" );
+var app = express();
+
+// todo "/" => "Hi there!"
+app.get( "/", function( request, response ) {
+  response.send( "Hi there!" );
+} )
+
+// "/bye" => "Goodbye!"
+app.get( "/bye", function( request, response ) {
+  response.send( "Goodbye!" );
+} )
+
+// "/dog" => "MEOW!"
+app.get( "/dog", function( request, response ) {
+  response.send( "Woof!" );
+} )
+
+app.get( "/cat", function( request, response ) {
+  response.send( "MEOW!" );
+} )
+
+app.listen( 3000, function() {
+  console.log( "The server has started on port 3000" );
+} );
