3e0f7f73c3a93eb8ce7a0701900edac7b75ebd1f
Glen Young <glen@geekpulp.co.nz>
2019-03-17 11:57:53 +1300

    Update app.js
    Add example of route parameters

--- Node/IntroToExpress/FirstExpressApp/app.js
+++ Node/IntroToExpress/FirstExpressApp/app.js
@@ -20,6 +20,20 @@ app.get( "/cat", function( request, response ) {
   response.send( "MEOW!" );
 } )
 
+// This example shows the use or route parameters. This allows us to created dynamic routes
+
+app.get( "/blog/:date/:title/", function( request, response ) {
+  var blogPostDate = request.params.date;
+  var blogPostTitle = request.params.title;
+  response.send( blogPostTitle + " Published " + blogPostDate );
+} )
+
+app.get( "*", function( request, response ) {
+  response.send( "Splat!" );
+} )
+
+// Tells express to listen for requests (Start server)
+
 app.listen( 3000, function() {
-  console.log( "The server has started on port 3000" );
+  console.log( "The server started on http://localhost:3000/" );
 } );
