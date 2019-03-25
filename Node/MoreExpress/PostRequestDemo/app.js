9a8133c9951f326f3b017e70164aef4b62d08a49
Glen Young <glen@geekpulp.co.nz>
2019-03-25 17:23:15 +1300

    Complete section 26

--- Node/MoreExpress/PostRequestDemo/app.js
+++ Node/MoreExpress/PostRequestDemo/app.js
@@ -3,7 +3,11 @@
 var express = require( "express" );
 var app = express();
 var bodyParser = require( "body-parser" );
+let teams = [ "Planning and Transformation", "Digital", "Application Development", "Application Support", "Infrustcture", "Service Desk", "Business Systems Support", "Architecture" ];
 
+app.use( bodyParser.urlencoded( {
+  extended: true
+} ) );
 app.set( "view engine", "ejs" );
 
 app.get( "/", function( request, response ) {
@@ -11,15 +15,15 @@ app.get( "/", function( request, response ) {
 } )
 
 app.get( "/teams", function( request, response ) {
-  let teams = [ "Planning and Transformation", "Digital", "Application Development", "Application Support", "Infrustcture", "Service Desk", "Business Systems Support", "Architecture" ];
   response.render( "teams", {
     teams: teams
   } );
 } )
 
 app.post( "/addteam", function( request, response ) {
-  console.log( request.body );
-  response.send( "You have reached the post route" )
+  let newTeam = request.body.newTeam;
+  teams.push( newTeam );
+  response.redirect( "/teams" );
 } );
 
 app.get( "*", function( request, response ) {
