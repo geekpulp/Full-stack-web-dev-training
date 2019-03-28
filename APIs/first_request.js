3af49ba38dbd563e538652d07e31857835285973
Glen Young <glen@geekpulp.co.nz>
2019-03-29 07:02:15 +1300

    Update first_request.js

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,11 +1,9 @@
-var request = require( "request" );
-request( "http://www.geekpulp.co.nz", function( error, response, body ) {
-  if ( error ) {
-    console.log( "Yikes!" );
-    console.log( error );
-  } else {
-    if ( response.statusCode === 200 ) {
-      console.log( body );
-    }
+"use strict"
+const request = require( "request" );
+
+request( "https://jsonplaceholder.typicode.com/users/1", function( error, response, body ) {
+  if ( !error && response.statusCode == 200 ) {
+    const parsedUserData = JSON.parse( body );
+    console.log( parsedUserData.name );
   }
 } );
