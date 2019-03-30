1e9d8afa216863e8df19b656a2abfeebf7dfa63a
Glen Young <glen@geekpulp.co.nz>
2019-03-31 07:50:47 +1300

    Add completed API examples

--- APIs/movie-search-app/app.js
+++ APIs/movie-search-app/app.js
@@ -5,12 +5,14 @@ var app = express();
 var request = require( "request" );
 app.set( "view engine", "ejs" );
 
-app.get( "/", function( request, response ) {
-  response.send( "home" );
+app.get( "/", function( req, res ) {
+  res.render( "search" );
 } )
 
 app.get( "/results", function( req, res ) {
-  request( "http://omdbapi.com/?s=cake&apikey=thewdb", function( error, response, body ) {
+  const query = req.query.search;
+  const url = "http://omdbapi.com/?s=" + query + "&apikey=thewdb";
+  request( url, function( error, response, body ) {
     if ( !error && response.statusCode == 200 ) {
       const parsedData = JSON.parse( body );
       // res.send( parsedData[ "Search" ][ 0 ][ "Title" ] );
