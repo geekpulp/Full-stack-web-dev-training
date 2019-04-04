7da21f91f47106ed0ff52299221a2945549eeace
Glen Young <glen@geekpulp.co.nz>
2019-04-04 22:42:38 +1300

    add new campground form

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -2,7 +2,24 @@
 
 var express = require( "express" );
 var app = express();
+var bodyParser = require( 'body-parser' );
+let campgrounds = [ {
+  name: "Palmerston North Holiday Park",
+  image: "https://www.holidayparks.co.nz/sites/default/files/styles/park_gallery/public/parks/Palm%20North%20Holiday%20Park%20004.jpg"
+}, {
+  name: "Feilding Holiday Park",
+  image: "https://cdn.tribalogic.net/uploads/0e/435036-feilding-holiday-park-1.jpg"
+}, {
+  name: "Himatangi Holiday Park",
+  image: "https://www.holidayparks.co.nz/sites/default/files/styles/park_gallery/public/parks/Camp%20photo%20from%20airplane.jpg"
+}, {
+  name: "Foxton Beach Top 10 Holiday Park",
+  image: "https://media-cdn.tripadvisor.com/media/photo-s/04/2c/a5/fc/foxton-beach-motor-camp.jpg"
+} ];
 
+app.use( bodyParser.urlencoded( {
+  extended: true
+} ) );
 app.set( "view engine", "ejs" );
 
 app.get( "/", function( req, res ) {
@@ -10,22 +27,26 @@ app.get( "/", function( req, res ) {
 } );
 
 app.get( "/campgrounds", function( req, res ) {
-  const campgrounds = [ {
-    name: "Palmerston North Holiday Park",
-    image: "https://www.holidayparks.co.nz/sites/default/files/styles/park_gallery/public/parks/Palm%20North%20Holiday%20Park%20004.jpg"
-  }, {
-    name: "Feilding Holiday Park",
-    image: "https://cdn.tribalogic.net/uploads/0e/435036-feilding-holiday-park-1.jpg"
-  }, {
-    name: "Himatangi Holiday Park",
-    image: "https://www.holidayparks.co.nz/sites/default/files/styles/park_gallery/public/parks/Camp%20photo%20from%20airplane.jpg"
-  }, {
-    name: "Foxton Beach Top 10 Holiday Park",
-    image: "https://media-cdn.tripadvisor.com/media/photo-s/04/2c/a5/fc/foxton-beach-motor-camp.jpg"
-  } ]
   res.render( "campgrounds", {
     campgrounds: campgrounds
   } );
+} );
+
+app.post( "/campgrounds", function( req, res ) {
+  // get data from form and add back to campgound array
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {
+    name: name,
+    image: image
+  }
+  campgrounds.push( newCampground );
+  // redirect back to campgrounds
+  res.redirect( "/campgrounds" );
+} );
+
+app.get( "/campgrounds/new", function( req, res ) {
+  res.render( "new" );
 } );
 
 app.get( "*", function( req, res ) {
