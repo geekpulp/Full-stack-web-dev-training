9e8cf11e686690da6459105211c19c1dcda40ddc
Glen Young <glen@geekpulp.co.nz>
2019-04-11 06:50:14 +1200

    Add cats DB example
    Using mongoose to create a schema for a cat then creating a model of 
    that schema so we can use mongoose methods to create and find data in 
    the cat DB

--- Databases/cats.js
+++ Databases/cats.js
@@ -6,11 +6,49 @@ mongoose.connect( 'mongodb://localhost/cat-app', {
 var catSchema = new mongoose.Schema( {
   name: String,
   age: Number,
-  temerament: String
+  temperament: String
 } );
 
-var Cat = mongoose.model( "Cat", catSchema );
+let Cat = mongoose.model( "Cat", catSchema );
 
 // Adding a new cat to the DB
 
+// let newCat = new Cat( {
+//   name: "Mrs. Norris",
+//   age: 311,
+//   temperament: "Evil"
+// } );
+//
+// newCat.save( function( err, cat ) {
+//   if ( err ) {
+//     console.log( "Yikes something went wrong" );
+//   } else {
+//     console.log( "We saved a cat" );
+//     console.log( cat );
+//   }
+// } );
+
+Cat.create( {
+  name: "Snow White",
+  age: 15,
+  temperament: "Bland"
+}, function( err, cat ) {
+  if ( err ) {
+    console.log( err );
+  } else {
+    console.log( cat );
+  }
+} );
+
+
 // retrieve all cats formt he DB and consle.log each one
+
+Cat.find( {}, function( err, cats ) {
+  if ( err ) {
+    console.log( "Danger Glen Young, Danger" );
+    console.log( err );
+  } else {
+    console.log( "All of the cats all of the time" )
+    console.log( cats );
+  }
+} );
