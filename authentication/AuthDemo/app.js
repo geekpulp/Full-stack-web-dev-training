d28e40110b5eaf1ef94ee5b6814652ad0c59c8fe
Glen Young <glen@geekpulp.co.nz>
2019-06-04 22:00:15 +1200

    add passport module

--- authentication/AuthDemo/app.js
+++ authentication/AuthDemo/app.js
@@ -4,16 +4,32 @@
 const bodyParser = require( "body-parser" ),
   mongoose = require( "mongoose" ),
   express = require( "express" ),
+  passport = require( "passport" ),
+  localStrategy = require( "passport-local" ),
+  passportLocalMongoose = require( "passport-local-mongoose" ),
+  user = require( "./models/user" ),
   app = express();
 
 mongoose.connect( "mongodb://localhost/authDemo", {
   useNewUrlParser: true
 } );
+
 app.set( "view engine", "ejs" );
+
+app.use( require( "express-session" )( {
+  secret: "Emma and Alex are the best kids in all the world",
+  resave: false,
+  saveUninitialized: false
+} ) );
 app.use( express.static( "public" ) );
 app.use( bodyParser.urlencoded( {
   extended: true
 } ) );
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+passport.serializeUser( user.serializeUser() );
+passport.deserializeUser( user.deserializeUser() );
 
 app.get( "/", function( req, res ) {
   res.render( "home" );
