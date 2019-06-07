7770434e3d69394240eb11fc6dad2716b2af565a
Glen Young <glen@geekpulp.co.nz>
2019-06-07 16:43:47 +1200

    Add user registration routes

--- authentication/AuthDemo/app.js
+++ authentication/AuthDemo/app.js
@@ -7,7 +7,7 @@ const bodyParser = require( "body-parser" ),
   passport = require( "passport" ),
   localStrategy = require( "passport-local" ),
   passportLocalMongoose = require( "passport-local-mongoose" ),
-  user = require( "./models/user" ),
+  User = require( "./models/user" ),
   app = express();
 
 mongoose.connect( "mongodb://localhost/authDemo", {
@@ -16,6 +16,9 @@ mongoose.connect( "mongodb://localhost/authDemo", {
 
 app.set( "view engine", "ejs" );
 
+app.use( bodyParser.urlencoded( {
+  extended: true
+} ) );
 app.use( require( "express-session" )( {
   secret: "Emma and Alex are the best kids in all the world",
   resave: false,
@@ -28,8 +31,12 @@ app.use( bodyParser.urlencoded( {
 app.use( passport.initialize() );
 app.use( passport.session() );
 
-passport.serializeUser( user.serializeUser() );
-passport.deserializeUser( user.deserializeUser() );
+passport.serializeUser( User.serializeUser() );
+passport.deserializeUser( User.deserializeUser() );
+
+// =====================
+// ROUTES
+// =====================
 
 app.get( "/", function( req, res ) {
   res.render( "home" );
@@ -37,6 +44,31 @@ app.get( "/", function( req, res ) {
 
 app.get( "/secret", function( req, res ) {
   res.render( "secret" );
+} );
+
+// =====================
+// Autentication routes
+// =====================
+
+app.get( "/register", function( req, res ) {
+  res.render( "register" );
+} );
+
+app.post( "/register", function( req, res ) {
+  req.body.username
+  req.body.password
+  User.register( new User( {
+    username: req.body.username
+  } ), req.body.password, function( err, user ) {
+    if ( err ) {
+      console.log( err );
+      return res.render( "register" );
+    } else {
+      passport.authenticate( "local" )( req, res, function() {
+        res.redirect( "/secret" );
+      } );
+    }
+  } );
 } );
 
 app.get( "*", function( req, res ) {
