249af7b6873d9608747cc56284c3a803e3157117
Glen Young <glen@geekpulp.co.nz>
2019-06-20 20:15:47 +1200

    Add initial code for user authentication

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -4,8 +4,11 @@ const express = require( "express" ),
   app = express(),
   bodyParser = require( 'body-parser' ),
   mongoose = require( "mongoose" ),
+  passport = require( "passport" ),
+  LocalStrategy = require( "passport-local" ),
   Campground = require( "./models/campground" ),
   Comment = require( "./models/comment" ),
+  User = require( "./models/user" ),
   seedDB = require( "./seeds" )
 
 mongoose.connect( "mongodb://localhost/yelp-camp", {
