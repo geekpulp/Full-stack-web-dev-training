e3b7b4b299fe8127bb2732da512104efbcd5ca2e
Glen Young <glen@geekpulp.co.nz>
2019-07-01 21:41:37 +1200

    Remove redundent code

--- JS/logan-homework-help/script.js
+++ JS/logan-homework-help/script.js
@@ -1,5 +1,6 @@
 "strict"
 
+//Creates a Dictionary for CPUs
 const cpus = [ {
     type: "CoreI3",
     brand: "Intel"
@@ -30,14 +31,16 @@ const cpus = [ {
   }
 ]
 
+//Sets the score to 0 at the start of the quiz
 let score = 0;
 
-
+//Steps through each value the dictonary asking a question of each
 cpus.forEach( function( cpu ) {
-  let answerOption = Math.random();
   let answer = prompt( "What brand created " + cpu.type + "?" )
+  //If the user cancels then thank for playing
   if ( answer == null || answer == "" ) {
     alert( "Thanks for playing" );
+    //If they answer check to see if they are correct or not. Display result and score, update score if correct
   } else {
     if ( answer == cpu.brand ) {
       score = score + 1;
